0ce0b127968f0d86f2296af32e64b41d31948148
Sophie <sophie.gylstorff@uni-ulm.de>
2020-11-18 23:36:49 +0100

    fixed post there weres several problems: jquery resolving ids with # , server runs for now under http not https getElementsByName did not resolve values validate function did not return the value jQuery Post did not send data in correct format therefore used ajax also adapted server to parse json from requests

--- js/signUp.js
+++ js/signUp.js
@@ -1,25 +1,33 @@
-const Http = new XMLHttpRequest();
-const url='https://localhost:3000/adminData';
+const url='http://localhost:3000/adminData';
 
 $(document).ready(function(){
-    $('#btn').click(function () {
+    $("#btn1").click(function (){
+        const data = {
+            name: validate(document.getElementById("name").value),
+            password: validate(document.getElementById("password").value),
+            email: validate(document.getElementById("email").value)
+        };
+        ajaxPost(url, data);
     });
 });
 
+function ajaxPost(url, data){
+    $.ajax({
+        url: url,
+        method: "POST",
+        data: data,
+        success: function(data){
+            console.log('on success : '+data);
+        },
+        error: function(e){
+            console.log("login error, status: "+e.status +" message : "+e.responseText);
+        }
+    })
+}
 
 function validate(data) {
-    if(data ==="" ){
-        alert("Form must be filled out");
-    }
+ if(data ==="" ){
+     alert("Form must be filled out");
+   }
+ return data;
 }
-/*
-function (){
-    const data= {
-        name: validate(document.getElementsByName("name").value),
-        password: validate(document.getElementsByName("password").value),
-        email: validate(document.getElementsByName("email").value)
-    };
-    $.post(url,data,function(data,status){
-        console.log('${data}')
-    })
-// */
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,29 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(require('body-parser').urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
+    //TODO response with sth else?
   res.send('Hello Adventskalendar!');
 });
-app.post('/user',(req,res) =>{
-    //
-    res.send('Accepted');
+
+app.post('/adminData',(req,res) =>{
+    console.log("posted data:" + JSON.stringify(req.body));
+    //TODO validate input
+
+    //##This is added to reduce the warnings on client side
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //##
+
+    //Response:
+    res.status(200);
+    res.send('OK');
 });
+
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
 } );
