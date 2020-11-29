bd8b9c4729329b24db0a2631d8abf8e5903a7c6b
zui04 <zui04@uni-ulm.de>
2020-11-29 21:10:18 +0100

    Get something from server

---
+++ js/calenderDayClicked.js
@@ -0,0 +1,52 @@
+const url='http://localhost:3000/adminData';
+
+$(document).ready(function() {
+    $(".style_prevu_kit").click(function(event) {
+        //alert(event.target.id);
+        if(event.target.id != ""){
+            let dayID = event.target.id;
+
+            // send request
+            const data = {
+                dayID: dayID
+            };
+            ajaxPost(url, data);
+        }
+    });
+
+    $(".v16_102").click(function(event) {
+        //alert(event.target.innerText);
+        if( event.target.innerText != ""){
+            let dayID = "day" + event.target.innerText;
+            // send request
+            const data = {
+                dayID: dayID
+            };
+            ajaxPost(url, data);
+        }
+
+    });
+
+});
+
+function ajaxPost(url, data){
+    $.ajax({
+        url: url,
+        //contentType: "image/jpeg",
+        method: "POST",
+        data: data,
+        success: function(data){
+            console.log('on success : '+ data);
+
+            //$('.modal-body').html(data);
+            //$('#empModal').modal('show');
+
+
+        },
+        error: function(e){
+            console.log("login error, status: "+e.status +" message : "+e.responseText);
+
+        }
+    })
+}
+
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import {log} from "util";
 
 const app = express();
 const port = 3000;
@@ -11,9 +12,20 @@ app.get('/', (req, res) => {
   res.send('Hello Adventskalendar!');
 });
 
-app.post('/adminData',(req,res) =>{
+app.post('/adminData',(req,res) => {
     console.log("posted data:" + JSON.stringify(req.body));
     //TODO validate input
+
+
+    let clickedDayInt = clickedDayAsInt({dataClient: JSON.stringify(req.body)});
+
+    let resBool = checkDate({clicked_dd: clickedDayInt});
+
+    let path = "";
+    if (resBool == true) {
+        //get Filepath
+        path = getPictureFilePath({clickedDayInt: clickedDayInt});
+    }
 
     //##This is added to reduce the warnings on client side
     res.header("Access-Control-Allow-Origin", "*");
@@ -22,9 +34,53 @@ app.post('/adminData',(req,res) =>{
 
     //Response:
     res.status(200);
-    res.send('OK');
+    //res.send(res_text);
+    res.sendFile(path, function (err) {
+        if (err) {
+            console.error(err);
+            res.status(401);
+        }
+    });
 });
+
 
 app.listen( port, () => {
     console.log( `server started at http://localhost:${ port }` );
 } );
+
+
+
+function checkDate({clicked_dd}: { clicked_dd: any }){
+
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    //let yyyy = today.getFullYear();
+
+    // if(mm != "11"){
+    //     return false;
+    // }
+
+
+    // check date
+    if(parseInt(clicked_dd) <= parseInt(dd) ){
+        return true;
+    }
+    return false;
+}
+
+
+function clickedDayAsInt({dataClient}: { dataClient: any }) {
+
+    let jsonDataClient = JSON.parse(dataClient);
+
+    let clicked_dayId = jsonDataClient.dayID;
+    let clicked_dayInt = clicked_dayId.substring(3);
+
+    return clicked_dayInt;
+}
+
+function getPictureFilePath({clickedDayInt}: { clickedDayInt: any }) {
+    return "/Users/universitat/WebstormProjects/PersonalWebAdventskalender/server/src/pictures/"+ clickedDayInt +".jpeg";
+
+}
